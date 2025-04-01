65731ddbe376525ea4c133a1b5629dc24005ff28
damienrigaudeau <91134839+damienrigaudeau@users.noreply.github.com>
2025-04-01 14:54:27 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,13 +1,12 @@
 from flask import Flask, render_template_string, render_template, jsonify
- from flask import render_template
- from flask import json
- from datetime import datetime
- from urllib.request import urlopen
- import sqlite3
-
-app = Flask(__name__)    
-
-                                                                                                     
+from flask import render_template
+from flask import json
+from datetime import datetime
+from urllib.request import urlopen
+import sqlite3
+                                                                                                         
+app = Flask(__name__)                                                                                                                  
+                                                                                                                                       
 @app.route('/')
 def hello_world():
     return render_template('hello.html')
@@ -36,8 +35,10 @@ def mongraphique():
 @app.route("/histogramme/")
 def exercice4():
     return render_template("histogramme.html")
-  
+
+@app.route('/commits/')
+def commits_page():
+    return render_template('commits.html')
   
 if __name__ == "__main__":
   app.run(debug=True)
-  
