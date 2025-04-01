20c5a8a177950c5b9cca733f65ac316d6ba7160d
damienrigaudeau <91134839+damienrigaudeau@users.noreply.github.com>
2025-04-01 14:35:26 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -40,29 +40,6 @@ def exercice4():
 def commits_page():
     return render_template('commits.html')
 
-import requests
-from datetime import datetime
-from flask import jsonify
-
-@app.route('/api/commits-data/')
-def commits_data():
-    url = 'https://api.github.com/repos/OpenRSI/5MCSI_Metriques/commits'
-    response = requests.get(url)
-    commits = response.json()
-
-    commit_minutes = {}
-
-    for commit in commits:
-        try:
-            date_str = commit['commit']['author']['date']
-            dt = datetime.strptime(date_str, '%Y-%m-%dT%H:%M:%SZ')
-            minute = dt.minute
-            commit_minutes[minute] = commit_minutes.get(minute, 0) + 1
-        except Exception as e:
-            print("Erreur de parsing :", e)
-
-    return jsonify(commit_minutes)
-
   
 if __name__ == "__main__":
   app.run(debug=True)
