3f9cf9a769e13bf9d5312250a006aea3949a253a
damienrigaudeau <91134839+damienrigaudeau@users.noreply.github.com>
2025-04-01 13:54:37 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -21,18 +21,11 @@ def meteo():
     response = urlopen('https://samples.openweathermap.org/data/2.5/forecast?lat=0&lon=0&appid=xxx')
     raw_content = response.read()
     json_content = json.loads(raw_content.decode('utf-8'))
-
     results = []
     for list_element in json_content.get('list', []):
-        dt_value = list_element.get('dt')  # Timestamp brut
-        temp_kelvin = list_element.get('main', {}).get('temp')
-        if temp_kelvin is not None:
-            temp_celsius = temp_kelvin - 273.15  # Conversion
-            results.append({
-                'Jour': dt_value,
-                'Température (°C)': round(temp_celsius, 2)
-            })
-
+        dt_value = list_element.get('dt')
+        temp_day_value = list_element.get('main', {}).get('temp') - 273.15 # Conversion de Kelvin en °c 
+        results.append({'Jour': dt_value, 'temp': temp_day_value})
     return jsonify(results=results)
   
 if __name__ == "__main__":
