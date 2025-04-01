95d4ac09cf85304c1c131a2c53cbd19c2d4a02d6
damienrigaudeau <91134839+damienrigaudeau@users.noreply.github.com>
2025-04-01 14:52:42 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,10 +1,10 @@
 from flask import Flask, render_template_string, render_template, jsonify
-from flask import render_template
-from flask import json
-from datetime import datetime
-from urllib.request import urlopen
-import sqlite3
-                                                                                                         
+ from flask import render_template
+ from flask import json
+ from datetime import datetime
+ from urllib.request import urlopen
+ import sqlite3
+
 app = Flask(__name__)    
 
                                                                                                      
@@ -37,9 +37,6 @@ def mongraphique():
 def exercice4():
     return render_template("histogramme.html")
   
-@app.route('/commits/')
- def commits_page():
-     return render_template('commits.html')
   
 if __name__ == "__main__":
   app.run(debug=True)
